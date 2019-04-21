bcc6c2153602aa72e1b5e105d267731c24cd1005
Devin Price <devingprice@gmail.com>
2019-04-21 19:52:30 -0400

    Give control of svg.js to circle class so it can control the width/height

--- app.js
+++ app.js
@@ -187,6 +187,13 @@ class CircleMaze {
         circle.attr('stroke-dasharray', stroke)
     }
 
+    createSVG(targetId){
+        return SVG(targetId)
+            .attr('id', 'mazeSVG')
+            .size(500, 500)
+            .viewbox(0, 0, 1000, 1000);
+    }
+    
 }
 class Cell {
     constructor(x1, x2, y) {
@@ -246,9 +253,357 @@ function display(m) {
     return text.join('');
 }
 
-var genMaze = maze(3, 8);
-//var genMaze = maze(3, 8);
-console.log(display(genMaze))
 
-console.log(new CircleMaze(500, 500, genMaze, 100, 400))
+
+
+// var draw = SVG('maze')
+//         .attr('id', 'mazeSVG')
+//         // .size(500, 500)
+//         .size(500, 500)
+//         .viewbox(0, 0, 1000, 1000);
+
+
+
+// var genMaze = maze(3, 8);
+
+// console.log(display(genMaze))
+
 // console.log(new CircleMaze(500, 500, genMaze, 100, 400))
+
+// console.log(new CircleMaze(500, 500, genMaze, 100, 400))
+//var genMaze = maze(3, 8);
+
+
+
+
+/*
+maze
+params
+    current = x, y, maze, hallwayThickness, outerradius
+
+    maze (vertical walls + horizontal walls) (generate using function inside class)
+    width, height (x, y = half respectively [OR keep viewbox 1000 and half is always 500])
+    line thickness
+    numHallways
+    inner radius, outer radius (inner radius + outerradius + num hallways = hallwayThickness)
+*/
+
+
+
+
+
+function closeBoxListeners(closeId, mazeId, svgId){
+    document.getElementById('x-close').addEventListener('mouseover', function (e) {
+        document.getElementById('x-close').classList.add('close-left');
+        document.getElementById('maze').classList.add('show');
+        //z index on svg is higher so this only gets called first time
+    })
+
+    var svgMaze = document.getElementById('mazeSVG');
+    svgMaze.addEventListener('load', function () {
+        console.log(svgMaze)
+
+        var svgLines = svgMaze.querySelectorAll('.circle-line')
+
+        svgLines.forEach(function (svgLine) {
+            svgLine.addEventListener('mouseover', function (e) {
+                if (e.target && e.target.classList.contains('circle-line')) {
+                    console.log('hovered')
+                    console.log('failed')
+                    document.getElementById('x-close').classList.toggle('close-left');
+                    document.getElementById('maze').classList.toggle('maze-flipped')
+                }
+
+            })
+        })
+
+        //TODO: currently not working if multiple mazes, need to add to all and let hovering target it's parent maze
+        svgMaze.querySelector('.circle-target').addEventListener('mouseover', function (e) {
+            if (e.target && e.target.classList.contains('circle-target')) {
+                console.log('reached the end')
+                console.log('made it')
+            }
+
+        })
+    })
+}
+
+var draw; //TODO move to inside class
+class NewCircleMaze {
+    constructor(args) {
+        //constants based off viewport size
+        this.x = 500;
+        this.y = 500;
+
+
+        this.hallwayThickness = 100;
+        this.innerCircleRadius = 100;
+        this.outerCircleRadius = 400;
+        this.spokeNum = 8;
+        this.lineThickness = args.lineThickness || 10;
+        this.lineColor = args.lineColor || '#444';
+
+        this.numHallways = (this.outerCircleRadius - this.innerCircleRadius) / this.hallwayThickness;
+
+
+
+        //inputs
+
+        // this.x = x;
+        // this.y = y;
+        // this.innerCircleRadius = 100;
+        // this.outerCircleRadius = outerCircleRadius;//300;
+        // this.hallwayThickness = hallwayThickness;//100;
+        // this.spokeNum = 8;
+        // this.lineThickness = 10
+        
+
+        //calc'ed
+
+        // this.numHallways = (this.outerCircleRadius - this.innerCircleRadius) / this.hallwayThickness;
+
+        //maze
+        this.maze = this.createMaze(3,8);
+        this.logMaze();
+
+        draw = this.createSVG('maze', args.width, args.height);
+        //svg lines
+        this.drawMaze(this.maze);
+        
+        //this.grid = this.generateGrid();
+    }
+    // generateGrid() {
+    //     var grid = [];
+    //     for (var i = 0; i < this.numHallways; i++) {
+    //         var hallway = [];
+    //         for (var k = 0; k < this.spokeNum; k++) {
+    //             hallway.push(new Cell(k, k, i));
+    //         }
+    //         grid.push(hallway);
+    //     }
+    //     return grid;
+    // }
+
+    drawMaze(maze) {
+        for (var i = 0; i < this.numHallways + 1; i++) {
+            
+            var vertical = undefined;
+            if (i === 0) {
+                vertical = [];
+                vertical[0] = true;
+            } else if ( i === this.numHallways ) {
+                vertical = [];
+                vertical[this.spokeNum-1] = true;
+            } else  {
+                vertical = maze.verti[ i -1 ]
+            }
+            //console.log(i, vertical);
+            this.drawCircle(this.innerCircleRadius + i * this.hallwayThickness, vertical)
+        }
+        for (var i = 0; i < this.spokeNum; i++) {
+            var radians = 2 * Math.PI * ((i + 1) / this.spokeNum);
+            //console.log(radians)
+            var edgePoint = this.calculateCircleEdge(this.outerCircleRadius, radians);
+            var innerPoint = this.calculateCircleEdge(this.innerCircleRadius, radians);
+            var horiz = [];
+            for(var k =0; k< this.numHallways; k++){
+                horiz.push( maze.horiz[k][i])
+                
+            }
+            if( i === this.spokeNum -1){
+                //horiz[i] = undefined;
+                console.log(horiz)
+                this.drawLine(innerPoint, edgePoint)
+
+            } else {
+                this.drawLine(innerPoint, edgePoint, horiz)
+            }
+            
+            
+        }
+
+        draw.path("M" + this.x + " " + (this.y - this.innerCircleRadius) +
+            " a " + this.innerCircleRadius + " " + this.innerCircleRadius + " 0 0 1 0 " + this.innerCircleRadius * 2 +
+            " a " + this.innerCircleRadius + " " + this.innerCircleRadius + " 0 0 1 0 " + this.innerCircleRadius * -2)
+            .attr({
+                fill: 'transparent'
+            })
+            .addClass('circle-target')
+    }
+    calculateCircleEdge(radius, radians) {
+        var x = radius * Math.sin(radians);
+        var y = radius * Math.cos(radians);
+        return [x, -y];
+    }
+    drawLine(point1, point2, horiz) {
+        var line = draw.line(point1[0] + this.x, point1[1] + this.y,
+            point2[0] + this.x, point2[1] + this.y)
+            .attr({
+                stroke: this.lineColor,
+                'stroke-width': this.lineThickness
+            })
+            .addClass('circle-line')
+        if( horiz !== undefined){
+            this.calcLineStrokeDash(line, horiz);
+        }
+        
+    }
+    calcLineStrokeDash(line, horiz) {
+        var stroke = '';
+        var countConcat = concatSimilarWithCount(horiz);
+        for(var i =0; i < countConcat.length; i++){
+            if( i === 0 && countConcat[i].type === true){
+                line.attr('stroke-dashoffset', countConcat[i].number * this.hallwayThickness * -1)
+            } else {
+                stroke += countConcat[i].number * this.hallwayThickness + ' ';
+            }
+            
+        }
+        stroke += ' ' + this.hallwayThickness * this.numHallways;
+        line.attr('stroke-dasharray', stroke)
+    }
+    drawCircle(radius, vertical) {
+        var circle = draw.path("M" + this.x + " " + (this.y - radius) +
+            " a " + radius + " " + radius + " 0 0 1 0 " + radius * 2 +
+            " a " + radius + " " + radius + " 0 0 1 0 " + radius * -2)
+            .attr({
+                fill: 'none',
+                stroke: this.lineColor,
+                'stroke-width': this.lineThickness
+            })
+            .addClass('circle-line')
+        if (vertical !== undefined) {
+            var circum =(2 * Math.PI * radius) ;
+            var gap = circum / this.spokeNum;
+            this.calcCircleStrokeDash(circle, vertical, gap);
+        }
+    }
+    calcCircleStrokeDash(circle, vertical, gap) {
+        var emptySpace = gap;
+        if (emptySpace > 200){ 
+            console.log( ' too big ');
+            emptySpace = 200;
+            
+            var stroke = '';
+            var countConcat = concatSimilarWithCount(vertical);
+            for(var i =0; i < countConcat.length; i++){
+                if( i === 0 && countConcat[i].type === true){
+                    //circle.attr('stroke-dashoffset', countConcat[i].number * gap * -1)
+                } else {
+                    stroke += countConcat[i].number * gap + ' ';
+                }
+                
+            }
+            stroke += ' ' + gap * this.spokeNum;
+            circle.attr('stroke-dasharray', stroke)
+            return;
+        };
+
+        var stroke = '';
+        var countConcat = concatSimilarWithCount(vertical);
+        for(var i =0; i < countConcat.length; i++){
+            if( i === 0 && countConcat[i].type === true){
+                circle.attr('stroke-dashoffset', countConcat[i].number * gap * -1)
+            } else {
+                stroke += countConcat[i].number * gap + ' ';
+            }
+            
+        }
+        stroke += ' ' + gap * this.spokeNum;
+        circle.attr('stroke-dasharray', stroke)
+    }
+
+    createSVG(targetId, width, height){
+        return SVG(targetId)
+            .attr('id', 'mazeSVG')
+            .size(width, height)
+            .viewbox(0, 0, 1000, 1000);
+    }
+    
+    createMaze(x, y) {
+        var n = x * y - 1;
+        if (n < 0) { 
+            alert("illegal maze dimensions"); 
+            return; 
+        }
+        var horiz = []; 
+        for (var j = 0; j < x + 1; j++) {
+            horiz[j] = []
+        }
+        var verti = []; 
+        for (var j = 0; j < x + 1; j++) {
+            verti[j] = []
+        }
+        var here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)]
+        var path = [here];
+        var unvisited = [];
+        for (var j = 0; j < x + 2; j++) {
+            unvisited[j] = [];
+            for (var k = 0; k < y + 1; k++){
+                unvisited[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1));
+            }
+        }
+        var next;
+        while (0 < n) {
+            var potential = [[here[0] + 1, here[1]], [here[0], here[1] + 1],
+            [here[0] - 1, here[1]], [here[0], here[1] - 1]];
+            var neighbors = [];
+            for (var j = 0; j < 4; j++)
+    
+                if (unvisited[(potential[j][0] + 1) % (x + 2)][(potential[j][1] + 1) % (y + 1)])
+                    neighbors.push(potential[j]);
+                else {
+                    //console.log(potential[j] + ' not a neighbor to ' + here)
+                }
+            if (neighbors.length) {
+                n = n - 1;
+                next = neighbors[Math.floor(Math.random() * neighbors.length)];
+                unvisited[next[0] + 1][next[1] + 1] = false;
+                if (next[0] == here[0])
+                    horiz[next[0]][(next[1] + here[1] - 1) / 2] = true;
+                else
+                    verti[(next[0] + here[0] - 1) / 2][next[1]] = true;
+                path.push(here = next);
+            } else
+                here = path.pop();
+        }
+        console.log({ x: x, y: y, horiz: horiz, verti: verti })
+        return { x: x, y: y, horiz: horiz, verti: verti };
+    }
+    logMaze() {
+        var m = this.maze;
+        var text = [];
+        for (var j = 0; j < m.x * 2 + 1; j++) {
+            var line = [];
+            if (0 == j % 2)
+                for (var k = 0; k < m.y * 4 + 1; k++)
+                    if (0 == k % 4)
+                        line[k] = '+';
+                    else
+                        if (j > 0 && m.verti[j / 2 - 1][Math.floor(k / 4)])
+                            line[k] = ' ';
+                        else
+                            line[k] = '-';
+            else
+                for (var k = 0; k < m.y * 4 + 1; k++)
+                    if (0 == k % 4)
+                        if (k > 0 && m.horiz[(j - 1) / 2][k / 4 - 1])
+                            line[k] = ' ';
+                        else
+                            line[k] = '|';
+                    else
+                        line[k] = ' ';
+            if (0 == j) line[1] = line[2] = line[3] = ' ';
+            if (m.x * 2 - 1 == j) line[4 * m.y] = ' ';
+            text.push(line.join('') + '\r\n');
+        }
+    
+        //return text.join('');
+        console.log( text.join('') );
+    }
+    
+}
+
+new NewCircleMaze({width: 500, height: 500, lineThickness: 20, lineColor: 'black'})
+
+closeBoxListeners();
